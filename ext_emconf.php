e1517f4995e6b4bdaabe69db162ab717494368e2
dst14 <daniel.stange@gmail.com>
2013-08-27 08:14:31 +0200

    CSS Implementation for listView, Basic implementation for singleView

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,36 +11,32 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-        'title' => 'European Icons',
-        'description' => 'European Icons',
-        'category' => 'plugin',
-        'author' => 'Daniel Stange',
-        'author_email' => 'stange@gei.de',
-        'shy' => '',
-        'dependencies' => '',
-        'conflicts' => '',
-        'priority' => '',
-        'module' => '',
-        'state' => 'alpha',
-        'internal' => '',
-        'uploadfolder' => 0,
-        'createDirs' => 'uploads/user_europeanicons',
-        'modify_tables' => '',
-        'clearCacheOnLoad' => 0,
-        'lockType' => '',
-        'author_company' => '',
-        'version' => '0.0.2',
-        'constraints' => array(
-                'depends' => array(
-                ),
-                'conflicts' => array(
-                ),
-                'suggests' => array(
-                ),
-        ),
-        '_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"0366";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"97a2";s:14:"ext_tables.php";s:4:"30b3";s:14:"ext_tables.sql";s:4:"6906";s:32:"icon_user_europeanicons_icon.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"c00f";s:7:"tca.php";s:4:"af61";s:19:"doc/wizard_form.dat";s:4:"56de";s:20:"doc/wizard_form.html";s:4:"7835";s:33:"pi1/class.user_europeanicons_pi1.php";s:4:"a8a8";s:17:"pi1/locallang.xml";s:4:"60ff";s:24:"pi1/static/editorcfg.txt";s:4:"69a7";}',
-        'suggests' => array(
-        ),
+    'title' => 'European Icons',
+    'description' => 'European Icons',
+    'category' => 'plugin',
+    'author' => 'Daniel Stange',
+    'author_email' => 'stange@gei.de',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => 'uploads/user_europeanicons',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '0.1.0',
+    'constraints' => array(
+        'depends' => array(),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"0366";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"97a2";s:14:"ext_tables.php";s:4:"30b3";s:14:"ext_tables.sql";s:4:"6906";s:32:"icon_user_europeanicons_icon.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"c00f";s:7:"tca.php";s:4:"af61";s:19:"doc/wizard_form.dat";s:4:"56de";s:20:"doc/wizard_form.html";s:4:"7835";s:33:"pi1/class.user_europeanicons_pi1.php";s:4:"a8a8";s:17:"pi1/locallang.xml";s:4:"60ff";s:24:"pi1/static/editorcfg.txt";s:4:"69a7";}',
+    'suggests' => array(),
 );
 
 ?>
